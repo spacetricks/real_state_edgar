9b54e94c4c8db2d89160ceb3a350be77188c0d85
Edgar Correa <128587854+spacetricks@users.noreply.github.com>
2024-06-24 13:17:40 -0600

    ajax y json

--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,7 @@
+
+var holderProperties = document.getElementById("holder-properties");
+console.log(holderProperties);
+
 var endpoint = "http://localhost:5500/front/data/real-state-properties.json";
 var xhr = new XMLHttpRequest();
 //console.log(xhr);
@@ -10,10 +14,27 @@ xhr.send();
 xhr.addEventListener("load", dataLoaded);
 
 function dataLoaded(e){
-  
-    console.log(JSON.parse(e.target.responseText));
+    var jsonData = JSON.parse(e.target.responseText);
+    var htmlData = "";
+    
+    for(var i = 0; i< jsonData.data.length; i++){
+        var currentRow = jsonData.data [i];
+        htmlData += "<h3>" + currentRow.property + "</h3>" + 
+        
+        "<p>Location: " + currentRow.location+ "</p>" + 
+        "<p>Price: " + currentRow.price+ "</p>" + 
+        
+        "<button> Comprar </button>"
 
-}
+    }
+    holderProperties.innerHTML = htmlData;
+    console.log(htmlData);
+   }
 
+    
+
+ /*console.log(jsonData [0].property);
+    console.log(jsonData [1].location);
+    console.log(jsonData [2].price);*/
 
 
