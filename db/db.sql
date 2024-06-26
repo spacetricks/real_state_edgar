5498e3630578d0ffe428cd02b014484fec21aee4
Edgar Correa <128587854+spacetricks@users.noreply.github.com>
2024-06-26 13:44:25 -0600

    cambios sql y php

---
+++ db/db.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE realstate;
+USE realstate;
+
+create table properties(
+    property VARCHAR(255) not null,
+    location BOOLEAN not null,
+    price int(11) not null
+);
+
+INSERT INTO properties VALUES("house", true, 30000);
+INSERT INTO properties VALUES("apartment", true, 60000);
+INSERT INTO properties VALUES("parking", true, 46000);
+
+SELECT * FROM properties;
+
+SHOW DATABASES;
